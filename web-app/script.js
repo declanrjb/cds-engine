ae11561de92a35cb0afbb9ffe2ed0d26b52d03c0
declanrjb <declanrjb@gmail.com>
2026-08-19 00:30:48 -0400

    html processing prototype

---
+++ web-app/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log('hello world')
+    $.getJSON('web-app/data.json', function(data) {
+        $.each(data['236328'], function(index, entry) {
+            var data_block = $('<div class="data-block"></div>').appendTo('.content')
+            var data_heading = $('<div class="data-heading"></div>').appendTo(data_block)
+            var table_code = $('<div class="table-code">' + entry['table_code'] + '</span>').appendTo(data_heading)
+            var table_name = $('<div class="table-title">' + entry['table_title'] + '</span>').appendTo(data_heading)
+            var data_table = $('<div class="data-table">' + entry['table_html'] + '</div>').appendTo(data_block)
+        });
+        $('.separator').append('<div class="download-button">Download <i class="fa-solid fa-download"></i></div>')
+    });
+
+}); 
