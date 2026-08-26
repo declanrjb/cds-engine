ea98c43704f2770b1dbd18bfe061031360af478c
declanrjb <declanrjb@gmail.com>
2026-08-25 21:22:03 -0400

    more doc processing

--- scripts/make-coverage-matrix.R
+++ scripts/make-coverage-matrix.R
@@ -6,7 +6,7 @@ git_base_link <- 'https://github.com/declanrjb/cds-engine/blob/main/'
 files <- list.files('data/cds-docs', recursive=TRUE, full.names=TRUE)
 directory <- read_csv('data/ipeds/hd2025.csv')
 directory <- directory |> 
-  select(UNITID, INSTNM, STABBR) |>
+  select(UNITID, INSTNM, STABBR, WEBADDR, INSTSIZE) |>
   mutate(
     UNITID = as.character(UNITID)
   )
@@ -24,28 +24,40 @@ files$year <- files$file |>
 
 files$url <- paste(git_base_link, files$file, '?token=A2MPB4CGSY4ZJYD25HC6263KPAI7E', sep='')
 
+files <- files |>
+  left_join(directory, by=c('unitid' = 'UNITID'))
+
+# missing: https://public.flourish.studio/uploads/1447708/28c69406-d4a3-44a0-b2d9-1e6f4212b28a.svg
+# present: https://public.flourish.studio/uploads/1447708/01af279a-7cff-47af-99f0-1b12cb044687.svg
+
 files$anchor <- files$url |>
   lapply(function(url) {
-    str_glue('<a class="file" href="{url}"><i class="fa-solid fa-file-lines"></i></a>')
+    str_glue('https://public.flourish.studio/uploads/1447708/01af279a-7cff-47af-99f0-1b12cb044687.svg')
   }) |>
   unlist()
 
 files <- files |>
-  left_join(directory, by=c('unitid' = 'UNITID'))
+  mutate(
+    INSTNM = paste(INSTNM, ' (', STABBR, ')', sep=''),
+  )
 
 export_table <- files |>
   filter(year != 'no') |>
   filter(year >= 2020) |>
-  select(INSTNM, STABBR, year, anchor) |> 
-  group_by(INSTNM, STABBR, year) |>
+  select(INSTNM, year, anchor) |> 
+  group_by(INSTNM, year) |>
   summarize(anchor = first(anchor)) |>
-  pivot_wider(id_cols=c('INSTNM', 'STABBR'), names_from='year', values_from='anchor')
+  pivot_wider(id_cols=c('INSTNM'), names_from='year', values_from='anchor')
 
 export_table <- export_table %>%
-  replace(is.na(.), '<i class="fa-solid fa-upload file"></i>')
+  replace(is.na(.), 'https://public.flourish.studio/uploads/1447708/28c69406-d4a3-44a0-b2d9-1e6f4212b28a.svg') |>
+  rename(Institution = INSTNM)
 
 export_table |>
-  head(n=20) |>
-  kbl(escape=FALSE) |> 
-  kable_styling() |> 
-  cat(file = "df.html")
+  write.csv('data/viz/cds-search.csv', row.names=FALSE)
+
+# export_table |>
+#   head(n=20) |>
+#   kbl(escape=FALSE) |> 
+#   kable_styling() |> 
+#   cat(file = "df.html")
--- web-app/script.js
+++ web-app/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-    console.log('hello world')
+    $( "select" ).selectmenu();
+
+    const queryString = window.location.search;
+
+    // 2. Parse the parameters using URLSearchParams
+    const urlParams = new URLSearchParams(queryString);
+
+
+
     $.getJSON('web-app/data.json', function(data) {
-        $.each(data['236328'], function(index, entry) {
-            var data_block = $('<div class="data-block"></div>').appendTo('.parsed-doc')
-            var data_heading = $('<div class="data-heading"></div>').appendTo(data_block)
-            var table_code = $('<div class="table-code">' + entry['table_code'] + '</span>').appendTo(data_heading)
-            var table_name = $('<div class="table-title">' + entry['table_title'] + '</span>').appendTo(data_heading)
-            var data_table = $('<div class="data-table">' + entry['table_html'] + '</div>').appendTo(data_block)
-        });
-        $('.separator').append('<div class="download-button">Download <i class="fa-solid fa-download"></i></div>')
+        if (urlParams.get('unitid') in data) {
+            $.each(data[urlParams.get('unitid')], function(index, entry) {
+                var data_block = $('<div class="data-block"></div>').appendTo('.parsed-doc')
+                var data_heading = $('<div class="data-heading"></div>').appendTo(data_block)
+                var table_code = $('<div class="table-code">' + entry['table_code'] + '</span>').appendTo(data_heading)
+                var table_name = $('<div class="table-title">' + entry['table_title'] + '</span>').appendTo(data_heading)
+                var data_table = $('<div class="data-table">' + entry['table_html'] + '</div>').appendTo(data_block)
+            });
+            $('.separator').append('<div class="download-button">Download <i class="fa-solid fa-download"></i></div>')
+        } else {
+            $('.no-results-banner').css('display', 'block')
+        }
+        
     });
 
+    $('#pagination').css('display', 'none')
 }); 
